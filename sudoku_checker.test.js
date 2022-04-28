24aa4235f83c55249d506ddf7b209fa99aac444e
Meridith <meridith.junk@gmail.com>
2022-04-28 15:14:23 -0500

    clean up spacing

--- sudoku_checker.test.js
+++ sudoku_checker.test.js
@@ -4,8 +4,8 @@ describe("sudoku checker tests", () => {
     test("Returns true if given input is valid sudoku", () => {
         expect(checker()).toBe(true);
     });
+
     test("Returns false if given input is invalid sudoku", () => {
         expect(checker()).toBe(false);
     });
-
 });
