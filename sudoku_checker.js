09433cff39099690b6144fee2598b15e482dd7d1
Meridith <meridith.junk@gmail.com>
2022-04-28 15:40:21 -0500

    first line of sudoku

--- sudoku_checker.js
+++ sudoku_checker.js
@@ -1,5 +1,10 @@
-function checker() {
-    return true;
+function checker(sudoku) {
+    let sudokuSet = new Set();
+    for(let num = 0; num <= 8; num++)
+    {
+        sudokuSet.add(sudoku[0][num]);
+    }
+    return sudokuSet.size === 9;
 }
 
 module.exports = checker;
--- sudoku_checker.test.js
+++ sudoku_checker.test.js
@@ -1,11 +1,19 @@
 const checker = require("./sudoku_checker");
 
+const validSudoku = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+];
+
+
+const invalidSudoku = [
+    [1, 2, 2, 3, 4, 4, 7, 8, 9]
+];
 describe("sudoku checker tests", () => {
     test("Returns true if given input is valid sudoku", () => {
-        expect(checker()).toBe(true);
+        expect(checker(validSudoku)).toBe(true);
     });
 
     test("Returns false if given input is invalid sudoku", () => {
-        expect(checker()).toBe(false);
+        expect(checker(invalidSudoku)).toBe(false);
     });
 });
